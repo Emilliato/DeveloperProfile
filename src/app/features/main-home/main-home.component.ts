4ec2e76c518d42aa7c7fb33e0774e0771d58ac01
Emillio Itai Marambo <emilliomarambo@gmail.com>
2024-09-08 15:18:25 +0200

    #2 #1
    Hope page with animations

--- src/app/features/main-home/main-home.component.ts
+++ src/app/features/main-home/main-home.component.ts
@@ -8,10 +8,31 @@ import { Router } from '@angular/router';
 })
 export class MainHomeComponent implements OnInit {
 
-  ngOnInit(): void {
-  }
+  fullText = "5 years of turning complex challenges into seamless, cutting-edge solutions—one secure line of code at a time.";
+  typedText = "";
+  showButtons = false;
+  typingSpeed = 100;
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.startTyping();
+    }, 4500);
+  }
+
+  startTyping() {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < this.fullText.length) {
+        this.typedText += this.fullText.charAt(i);
+        i++;
+      } else {
+        clearInterval(typingInterval);
+        this.showButtons = true;
+      }
+    }, this.typingSpeed);
+  }
 
   viewResume() {
     this.router.navigate(['/resume']);
